17b40c4f58363703f81c38a480721c0a042e8e9a
meshankhosla <meshankhosla@gmail.com>
2021-01-23 13:33:28 -0800

    Got rid of errors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import { React, useState } from 'react';
 import GraphView from './GraphView/GraphView';
 import TableView from './TableView/TableView';
 import { Motor } from './data/testData';
 
 import { Container, Row, Col } from 'react-bootstrap';
 function App() {
+  const [testData, setTestData] = useState(getData());
   return (
     <div className="App">
       <Container>
@@ -22,7 +23,8 @@ function App() {
 }
 
 const getData = () => {
-    let m1 = Motor(Math.random(), 1, "Super motor", 100, 5);
+    let m1 = new Motor(Math.random(), 1, "Super motor", 100, 5);
+    return [m1];
 
 }
 export default App;
--- src/GraphView/GraphView.jsx
+++ src/GraphView/GraphView.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {} from ''
 
 export default function GraphView() {
     return <p>GraphView</p>
