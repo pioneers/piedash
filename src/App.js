0ecdcb626be0e522844d2328d20c323223e37b6b
meshankhosla <meshankhosla@gmail.com>
2021-01-27 20:36:34 -0800

    Created working TableView Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import { React, useState } from 'react';
+import { React, useEffect, useState } from 'react';
 import GraphView from './GraphView/GraphView';
 import TableView from './TableView/TableView';
 import { Motor, Sensor } from './data/testData';
 
 import { Container, Row, Col } from 'react-bootstrap';
+
 function App() {
-  const [testData, setTestData] = useState(getData());
+  const [testVelocity, setTestVelocity] = useState(100);
+  const [testData, setTestData] = useState(getData(testVelocity));
+//   setTestVelocity(testVelocity + 100);
+  
+//   useEffect(() => {
+//       setTimeout(() => {
+//           setTestVelocity(testVelocity + 100);
+//           console.log(testVelocity)
+//       }, 1000);
+//   })
+
   return (
     <div className="App">
       <Container>
@@ -22,12 +33,14 @@ function App() {
   );
 }
 
-const getData = () => {
-    let m1 = new Motor(0, Math.random(), "motor", 100, 5);
+const getData = (testVelocity) => {
+    let m1 = new Motor(0, Math.random(), "motor", testVelocity, 5);
     let m2 = new Motor(1, Math.random(), "motor", 15, 1);
-    let s1 = new Sensor(2, Math.random(), "sensor", 10)
-    let s2 = new Sensor(3, Math.random(), "sensor", 100 )
-    return [m1, s1, m2, s2];
+    let s1 = new Sensor(2, Math.random(), "sensor", 10);
+    let s2 = new Sensor(3, Math.random(), "sensor", 100 );
+    let s3 = new Sensor(4, Math.random(), "sensor", 30);
+    let s4 = new Sensor(5, Math.random(), "sensor");
+    return [m1, s1, m2, s2, s3, s4];
 
 }
 export default App;
--- src/TableView/PeripheralTable.jsx
+++ src/TableView/PeripheralTable.jsx
@@ -2,35 +2,40 @@ import React from "react";
 import { Table } from 'react-bootstrap';
 
 export default function PeripheralTable({ deviceName, data, key }) {
+    let updatedDevicename = deviceName.charAt(0).toUpperCase() + deviceName.substr(1).toLowerCase();
+;
+
     return (
-        <Table striped bordered condensed hover>
-            {/* <thead>
-                <tr>
-                    <th>#</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Username</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td colSpan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                </tr>
-            </tbody> */}
-        </Table>
+        <div>
+            <h1>{updatedDevicename + "s"}</h1>
+            <Table striped bordered condensed hover >
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        {
+                            Object.keys(data[0].params).map((param, i) => (
+                                <th key={i}>{param}</th>
+                            ))
+                        }
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map((device, i) => {
+                            return (
+                                <tr>
+                                    <td>{device.key}</td>
+                                    {
+                                        Object.keys(device.params).map((dataField, i) => (
+                                            <td key={i}>{device.params[dataField] || "No Data Found"}</td>
+                                        ))
+                                    }
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody> 
+            </Table>
+        </div>
     );
 }
